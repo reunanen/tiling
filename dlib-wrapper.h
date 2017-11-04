b4af76c74c14ff24a77b8e65b43198fff3cd431d
Juha Reunanen <juha.reunanen@tomaattinen.com>
2017-11-04 15:03:37 +0200

    Update dlib-wrapper

--- dlib-wrapper.h
+++ dlib-wrapper.h
@@ -4,24 +4,34 @@
 
 namespace tiling {
 
-std::vector<dlib::rectangle> get_tiles(int full_width, int full_height, const parameters& parameters)
+struct dlib_tile
+{
+    dlib::rectangle full_rect;
+    dlib::rectangle non_overlapping_rect;
+};
+
+dlib::rectangle to_dlib_rectangle(const rectangle& rect)
+{
+    return dlib::rectangle(
+        rect.top_left.x,
+        rect.top_left.y,
+        rect.top_left.x + rect.size.width - 1,
+        rect.top_left.y + rect.size.height - 1
+    );
+}
+
+std::vector<dlib_tile> get_tiles(int full_width, int full_height, const parameters& parameters)
 {
     const std::vector<tile> tiles = get_tiles(tiling::size(full_width, full_height), parameters);
 
-    std::vector<dlib::rectangle> rects(tiles.size());
+    std::vector<dlib_tile> output(tiles.size());
 
     for (int i = 0, end = tiles.size(); i < end; ++i) {
-        const tile& tile = tiles[i];
-        rects[i] =
-            dlib::rectangle(
-                tile.top_left.x,
-                tile.top_left.y,
-                tile.top_left.x + tile.size.width - 1,
-                tile.top_left.y + tile.size.height - 1
-            );
+        output[i].full_rect = to_dlib_rectangle(tiles[i].full_rect);
+        output[i].non_overlapping_rect = to_dlib_rectangle(tiles[i].non_overlapping_rect);
     }
 
-    return rects;
+    return output;
 }
 
 }
